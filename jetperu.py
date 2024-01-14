c9216a0383fa3c57ad597f79734a9c2de91570ee
Renzo Plasencia <148007927+renzo-plasencia@users.noreply.github.com>
2024-01-14 15:21:00 -0500

    Update jetperu.py

--- jetperu.py
+++ jetperu.py
@@ -1,36 +1,32 @@
+############# LIBRERÍAS #############
 from selenium import webdriver
 from datetime import date
 import pandas as pd
 
-root = 'D:/UDEMY/WEB SCRAPING/'
+############# VARIABLES #############
+root = 'D:/Ruta/Prueba'
 file = 'Dolar Precios.xlsx'
 ruta=root+file
-
 data = pd.read_excel(ruta)
-
-website = "https://jetperu.com.pe/"
-path = "C:/Users/Renzo Plasencia/Downloads/chromedriver.exe"
-
 today = str(date.today())
-driver = webdriver.Chrome(path) #Activa el driver
-driver.get(website) #con el driver abre la web
 
+############# WEB #############
+website = "https://pagina.com.pe/" #(se ocultó el nombre de la web)
+path = "C:/Users/Usuario/Downloads/chromedriver.exe" #Ruta de prueba
+driver = webdriver.Chrome(path)
+driver.get(website)
+
+############# ENCONTRAR ELEMENTOS EN LA WEB #############
 buy_rate = driver.find_element_by_xpath('//span[@id="buyRate"]').text
 sell_rate = driver.find_element_by_xpath('//span[@id="sellRate"]').text
-print('Compra '+today+"= "+ buy_rate)
-print('Venta '+today+"= "+sell_rate)
+driver.quit()
 
-driver.quit() #cerrar
-
-#Crear un nuevo DataFrame con los datos actuales
+############# DataFrame #############
 dic_valores= {
     'Fecha': [today],
     'Compra': [buy_rate],
     'Venta': [sell_rate]
 }
-
-print(dic_valores)
 new_data = pd.DataFrame(dic_valores)
 data = pd.concat([data, new_data], ignore_index=True) #Concatenar con el anterior
 data.to_excel(ruta, index=False) #Guardar
-
